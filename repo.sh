850cc458ab8c8839e97dc091ca4782b1723938e5
Mike Lindner <mikelindner@mail.com>
2021-08-18 13:54:46 +1000

    added script

--- repo.sh
+++ repo.sh
@@ -1,5 +1,5 @@
 if [ -d /share/CentOS/7.9.2009 ] ; then
-    rsync  -avSHP --delete --exclude={'local*','isos','cloud','atomic'} mirror.aarnet.edu.au/pub/centos/7.9.2009/ /share/CentOS/7.9.2009/
+    rsync  -avSHP --delete --exclude "local*"--exclude={'isos','cloud','atomic'} mirror.aarnet.edu.au/pub/centos/7.9.2009/ /share/CentOS/7.9.2009/
 else
     echo "Target directory /share/CentOS/7.9.2009 not present."
 fi
